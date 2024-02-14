a921a43315ec82408613fe5198695ab54ad06388
Rohun Baxi <rohun@Rohuns-Laptop.local>
2024-02-13 20:51:21 -0500

    first hello world

--- team-102-16/build.gradle.kts
+++ team-102-16/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.21"
+    application
 }
 
 group = "org.example"
---
+++ team-102-16/src/main/kotlin/main/kotlin.kt
@@ -0,0 +1,5 @@
+package main
+class Main
+fun main(args : Array<String>) {
+    println("Hello, Rohun!")
+}
