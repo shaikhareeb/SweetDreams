25b9858305160c1198a4387c3eede9ba7409c66a
Akshen Jasikumar <akshen.jasikumar@gmail.com>
2024-03-08 09:48:28 -0500

    fixed merge conflicts

--- SweetDreams/src/jvmMain/kotlin/userinterface/SettingsPage.kt
+++ SweetDreams/src/jvmMain/kotlin/userinterface/SettingsPage.kt
@@ -2,25 +2,27 @@ package userinterface
 
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.ui.graphics.Color
 import androidx.compose.material.Button
 import androidx.compose.material.Slider
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 class SettingsPage: Page() {
     lateinit var navBar : NavBar
-    lateinit var onDelete : () -> Unit
+    lateinit var onDelete : (String) -> String
     lateinit var onReset : (String) -> Unit
 
 
     @Composable
     override fun Content(){
         var sliderPosition = 100.toFloat()
+        var uid by remember { mutableStateOf("") }
         var pwd by remember { mutableStateOf("") }
+        var errorText by remember { mutableStateOf("") }
         Row(modifier = Modifier.fillMaxSize().border(2.dp, Color.Gray)) {
             // NavBar on the left with its own outline
             Column(
@@ -49,18 +51,20 @@ class SettingsPage: Page() {
                     onNameChange = { if (it.length <= 50) pwd = it },
                     "New Password"
                 )
-
+                TextFieldFormat(name = uid, isPwd = false, onNameChange = { if (it.length <= 50) uid = it }, "Re-enter id to delete account")
                 Button(onClick = {
-                    onDelete()
+                    errorText = onDelete(uid)
                 }) {
                     Text("Delete Account")
                 }
+                TextFieldFormat(name = pwd, isPwd = true, onNameChange = { if (it.length <= 50) pwd = it }, "New Password")
                 Button(onClick = {
                     onReset
                     (pwd)
                 }) {
                     Text("Reset Password")
                 }
+                Text(errorText)
             }
         }
     }
